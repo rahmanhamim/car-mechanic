1ea79dfafbff70e213f75434944bab07c0f9e4b8
rahmanhamim <rahmanhamiminfo@gmail.com>
2021-10-13 20:23:55 +0600

    firebase update

--- src/pages/Login/Firebase/firebase.config.js
+++ src/pages/Login/Firebase/firebase.config.js
@@ -8,13 +8,4 @@ const firebaseConfig = {
  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// const firebaseConfig = {
-//  apiKey: "AIzaSyCrFpiU8pWtCD3dVjyI23Jso8JaCjWtCC0",
-//  authDomain: "car-mechanic-rh.firebaseapp.com",
-//  projectId: "car-mechanic-rh",
-//  storageBucket: "car-mechanic-rh.appspot.com",
-//  messagingSenderId: "712249394516",
-//  appId: "1:712249394516:web:43348ef0c104d8871974c1",
-// };
-
 export default firebaseConfig;
